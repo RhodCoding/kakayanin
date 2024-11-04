348bcc1b378830b1995ab3e00b99332fac6c08d2
RhodCoding <joewee321@gmail.com>
2024-11-04 17:20:49 +0800

    Add Product List

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,15 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<!-- <main> Navigation -->
+	<!-- <nav>
+		<ul> 
+			<li><a href="/">home</a></li>
+			<li><a href="/">portfolio</a></li>
+			<li><a href="/">about</a></li>
+			<li><a href="/">contact</a></li>
+		</ul>
+	</nav>  -->
+	{@render children()}
+  <!-- </main> -->
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,24 @@
-<p>nigga</p>
+<div class="top-bg">
+
+</div>
+
+<div class="prod-list">
+    <p>product list</p>
+    <div class="bread-list">
+        <div class="bread">
+          <img src="" alt="">
+          <h4>spanish bread</h4>
+          <h6>4.00 Pesos</h6>
+        </div>
+        <div class="bread">
+          <img src="" alt="">
+          <h4>spanish bread</h4>
+          <h6>4.00 Pesos</h6>
+        </div>
+        <div class="bread">
+          <img src="" alt="">
+          <h4>spanish bread</h4>
+          <h6>4.00 Pesos</h6>
+        </div>
+    </div>
+</div>
